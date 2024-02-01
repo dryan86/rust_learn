73a81ff737e484a1c261f9f8368663df329f8140
dryan86 <88956452@qq.com>
2024-02-02 01:26:55 +0800

    修改kitties模块为poe，并添加benchmark代码

--- substrate-node-template-pro6/pallets/poe/src/mock.rs
+++ substrate-node-template-pro6/pallets/poe/src/mock.rs
@@ -1,12 +1,10 @@
-use crate as pallet_kitties;
-use frame_support::traits::{ConstU16, ConstU64};
+use crate as pallet_poe;
+use frame_support::traits::{ConstU16, ConstU32, ConstU64};
 use sp_core::H256;
 use sp_runtime::{
 	traits::{BlakeTwo256, IdentityLookup},
 	BuildStorage,
 };
-
-use pallet_insecure_randomness_collective_flip;
 
 type Block = frame_system::mocking::MockBlock<Test>;
 
@@ -15,8 +13,7 @@ frame_support::construct_runtime!(
 	pub enum Test
 	{
 		System: frame_system,
-		KittiesModule: pallet_kitties,
-		Randomness: pallet_insecure_randomness_collective_flip,
+		PoeModule: pallet_poe,
 	}
 );
 
@@ -46,23 +43,13 @@ impl frame_system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
-impl pallet_kitties::Config for Test {
+impl pallet_poe::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
-	type Randomness = Randomness;
+	type MaxClaimLength = ConstU32<10>;
 	type WeightInfo = ();
 }
 
-impl pallet_insecure_randomness_collective_flip::Config for Test {}
-
-// Build genesis storage according to the mock runtime.
+// BUild genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	let mut ext:sp_io::TestExternalities = frame_system::GenesisConfig::<Test>::default().build_storage().unwrap().into();
-	ext.execute_with(|| System::set_block_number(1));
-	ext
+	frame_system::GenesisConfig::<Test>::default().build_storage().unwrap().into()
 }
-
-// pub fn new_test_ext() -> sp_io::TestExternalities {
-// 	let mut ext: sp_io::TestExternalities = system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
-// 	ext.execute_with(|| System::set_block_number(1));
-// 	ext
-//    }
